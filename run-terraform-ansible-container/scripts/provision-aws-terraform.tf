ec215111bbdca8f64ceacbc49a0ea8657997cb23
Wilhelm Wonigkeit <wilhelm.wonigkeit@direktiv.io>
2024-02-01 16:25:43 -0800

    Updated

--- run-terraform-ansible-container/scripts/provision-aws-terraform.tf
+++ run-terraform-ansible-container/scripts/provision-aws-terraform.tf
@@ -14,10 +14,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0e812285fd54f7620"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "ExampleTerraformMachine"
   }
 }
