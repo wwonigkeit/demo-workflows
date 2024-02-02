9f542050d1e6da1db86e5b6a771d5549b85d87b8
Wilhelm Wonigkeit <wilhelm.wonigkeit@direktiv.io>
2024-02-01 21:03:16 -0800

    Updated

--- run-terraform-ansible-container/scripts/provision-aws-terraform.tf
+++ run-terraform-ansible-container/scripts/provision-aws-terraform.tf
@@ -9,8 +9,18 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+variable "aws_key" {
+  type = string
+}
+
+variable "aws_secret" {
+  type = string
+}
+
 provider "aws" {
   region  = "ap-southeast-2"
+  access_key = var.aws_key
+  secret_key = var.aws_secret
 }
 
 resource "aws_instance" "app_server" {
