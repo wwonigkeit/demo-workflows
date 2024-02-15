2b072af060e460f2f5604a0558bc51ea15b59773
Wilhelm Wonigkeit <wilhelm.wonigkeit@direktiv.io>
2024-02-14 18:28:32 -0800

    Run python code

--- run-docker-container/code/python_example.py
+++ run-docker-container/code/python_example.py
@@ -3,3 +3,6 @@ import requests
 
 response = requests.get("https://jsonplaceholder.typicode.com/todos")
 todos = json.loads(response.text)
+
+# Pretty Printing JSON string back
+print(json.dumps(todos, indent = 4, sort_keys=True))
